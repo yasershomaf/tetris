a57ffb76190ef56ccdeeaac2fb34aaa7a2d144db
Yaser Somaf <yasersomaf@yasers-mbp.home>
2021-09-23 21:11:51 +0200

    Created the main application state

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useReducer } from 'react';
 
 import IColor from '../../services/types';
 
@@ -8,10 +8,12 @@ import Box from '../box/box';
 import './app.css';
 
 const App = () => {
+	const [gameLevel, setGameLevel] = useState<number>(1);
+
 	const [gameStatus, setGameStatus] = useState<'started' | 'paused' | 'stopped'>('stopped');
 
 	const [grid, setGrid] = useState<(IColor | null)[][]>(Array.from(
-		{length: 20}, row => Array.from({length: 10}, boxColor => null)
+		{length: 20}, (row) => Array.from({length: 10}, (boxColor) => null)
 	));
 
 	const shapesRef = useRef<boolean[][][]>([
@@ -30,13 +32,120 @@ const App = () => {
 		red: Math.floor(Math.random() * 256),
 		green: Math.floor(Math.random() * 256),
 		blue: Math.floor(Math.random() * 256)
-	})
+	});
 
-	const [nextShape, setNextShape] = useState<boolean[][]>(getNewShape());
-	const [nextShapeRotation, setNextShapeRotation] = useState<number>(
-		Math.floor(Math.random() * 4) * 90
-	);
-	const [nextShapeColor, setNextShapeColor] = useState<IColor>(getNewColor());
+	const [state, dispatch] = useReducer((state: any, action: any) => {
+		const rotation = state.current.rotation % 360;
+
+		let actualShape = state.current.shape;
+
+		const noOfRows = actualShape.length;
+		const noOcColumns = actualShape[0].length
+
+		if (rotation === 90) {
+			actualShape = Array.from({length: noOcColumns}, (
+				row, rowIndex
+			) => Array.from({length: noOfRows}, (
+				col, colIndex
+			) => actualShape[noOfRows - colIndex - 1][rowIndex]));
+		} else if (rotation === 270) {
+			actualShape = Array.from({length: noOcColumns}, (
+				row, rowIndex
+			) => Array.from({length: noOfRows}, (
+				col, colIndex
+			) => actualShape[colIndex][noOcColumns - rowIndex - 1]));
+		} else if (rotation === 180) {
+			actualShape = Array.from({length: noOfRows}, (
+				row, rowIndex
+			) => Array.from({length: noOcColumns}, (
+				col, colIndex
+			) => actualShape[noOfRows - rowIndex - 1][noOcColumns - colIndex - 1]));
+		}
+
+		let actualTop = state.current.top;
+		let actualLeft = state.current.left;
+
+		if (rotation === 90 || rotation === 270) {
+			actualTop = actualTop + (noOfRows - noOcColumns) / 2;
+			actualLeft = actualLeft + (noOcColumns - noOfRows) / 2;
+		}
+
+		switch(action.type) {
+			case 'rotate':
+				return {
+					...state,
+					current: {
+						...state.current,
+						rotation: state.current.rotation + 90
+					}
+				};
+			case 'downWards':
+				return {
+					...state,
+					current: {
+						...state.current,
+						top: state.current.top + 1
+					}
+				};
+			case 'leftWards':
+				return {
+					...state,
+					current: {
+						...state.current,
+						left: state.current.left - 1
+					}
+				};
+			case 'rightWards':
+				return {
+					...state,
+					current: {
+						...state.current,
+						left: state.current.left + 1
+					}
+				};
+			case 'setShape':
+				const isRotated = state.next.rotation % 180 > 0;
+				const shapeWidth = state.next.shape[0].length;
+				const shapeHeight = state.next.shape.length;
+				return {
+					current: {
+						...state.next,
+						top: -(!isRotated ? shapeHeight : shapeWidth),
+						left: Math.floor((10 - (isRotated ? shapeHeight : shapeWidth)) / 2)
+					},
+					next: {
+						shape: getNewShape(),
+						color: getNewColor(),
+						rotation: Math.floor(Math.random() * 4) * 90
+					}
+				};
+		}
+	}, {
+		current: {
+			shape: null,
+			color: null,
+			rotation: 0,
+			top: 0,
+			left: 0
+		},
+		next: {
+			shape: getNewShape(),
+			color: getNewColor(),
+			rotation: Math.floor(Math.random() * 4) * 90
+		}
+	});
+
+	const gameInterval = useRef<NodeJS.Timer | null>(null);
+
+	const startHandler = () => {
+		dispatch({type: 'setShape'});
+
+		gameInterval.current = setInterval(() => {
+			dispatch({type: 'downWards'});
+		}, 600 - 100 * gameLevel);
+
+		setGameStatus('started');
+	};
 
 	return <div className="container">
 		<div className="game-area">
@@ -46,16 +155,16 @@ const App = () => {
 					color={boxColor}
 				/> : <div key={boxIndex}></div>)}
 			</div>)}
+
+			{gameStatus === 'started' && <Shape {...state.current} />}
 		</div>
 
 		<div className="game-info">
 			<div className="next-shape">
 				<Shape
-					shape={nextShape}
-					color={nextShapeColor}
-					rotation={nextShapeRotation}
-					left={(7 - nextShape[0].length) / 2}
-					top={(7 - nextShape.length) / 2}
+					{...state.next}
+					left={(7 - state.next.shape[0].length) / 2}
+					top={(7 - state.next.shape.length) / 2}
 				/>
 			</div>
 
@@ -70,6 +179,7 @@ const App = () => {
 
 			{gameStatus === 'stopped' && <button
 				className="game-button start-button"
+				onClick={startHandler}
 			>Start</button>}
 
 			{gameStatus === 'started' && <button
--- src/services/types.ts
+++ src/services/types.ts
@@ -3,3 +3,19 @@ export default interface IColor {
 	green: number;
 	blue: number;
 };
+
+export interface IShape {
+	shape: boolean[][][];
+	color: IColor;
+	rotation: number;
+};
+
+export interface IShapeWithPosition extends IShape {
+	top: number;
+	left: number;
+};
+
+export interface IShapeStore {
+	current: IShapeWithPosition;
+	next: IShape;
+};
